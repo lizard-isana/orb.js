32a94b4b365831ea9890a0e6de97e33e3590afb4
Isana Kashiwai <isana.k@gmail.com>
2020-01-14 13:37:40 +0900

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
+
+const src = {
+  all: [
+    './src/core.js',
+    './src/time.js',
+    './src/earth.js',
+    './src/coordinates.js',
+    './src/observation.js',
+    './src/sgp4.js',
+    './src/kepler.js',
+    './src/sun.js',
+    './src/luna.js',
+    './src/vsop.js'
+  ],
+  core: [
+    './src/core.js',
+    './src/time.js',
+    './src/earth.js',
+    './src/coordinates.js',
+    './src/observation.js'
+  ],
+  planetary: [
+    './src/kepler.js',
+    './src/sun.js',
+    './src/luna.js',
+    './src/vsop.js'
+  ],
+  satellite: [
+    './src/sgp4.js'
+  ]
+}
+
+const concat = function (done) {
+  gulp.src(src.all)
+    .pipe(concat('orb.v2.js'))
+    .pipe(src.gulp.dest('./build'));
+
+  gulp.src(src.core)
+    .pipe(concat('orb-core.v2.js'))
+    .pipe(gulp.dest('./build'));
+
+  gulp.src(src.planetary)
+    .pipe(concat('orb-planetary.v2.js'))
+    .pipe(gulp.dest('./build'));
+
+  gulp.src(src.satellite)
+    .pipe(concat('orb-satellite.v2.js'))
+    .pipe(gulp.dest('./build'));
+  done()
+  return
+}
+
+const copy = function (done) {
+  var supplement = [
+    "src/orb-date-handler.js",
+    "src/orb-data-handler.js"
+  ]
+  return gulp.src(supplement)
+    .pipe(gulp.dest('./build'));
+};
+
+
+const minify = function (done) {
+
+  return gulp.src("./build/*.js")
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('./build/min'));
+};
+
+exports.build = gulp.series(concat, copy, minify);
