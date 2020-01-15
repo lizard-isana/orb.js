277ce9aa6aceef03f8a929aa13056bbf47bd7057
Isana Kashiwai <isana.k@gmail.com>
2020-01-15 14:40:51 +0900

    version number

--- src/orb.js
+++ src/orb.js
@@ -12,5 +12,8 @@
 // and the revision number is incremented when minor bugs are fixed.
 
 // for Name Space
-var Orb = Orb || {};
-
+var Orb = Orb || {
+  VERSION: "2.3.0 (20200115)",
+  AUTHOR: "Isana Kashiwai",
+  LICENSE: "MIT"
+};
