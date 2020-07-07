e3606a52938aee1e069b12343037bc3f288134e5
Isana Kashiwai <isana.k@gmail.com>
2020-07-07 11:13:54 +0900

    v2.4.0

--- build/orb-planetary.v2.js
+++ build/orb-planetary.v2.js
@@ -1,6 +1,6 @@
 // orb.js
 //
-// Orb 2.3.1 (20200124) - Javascript Library for Astronomical Calculation
+// Orb 2.4.0 (20200707) - Javascript Library for Astronomical Calculation
 //
 // Copyright (c) 2012 - 2020 KASHIWAI, Isana
 // Licensed under the MIT license (MIT-LICENSE),
@@ -13,7 +13,7 @@
 
 // for Name Space
 var Orb = Orb || {
-  VERSION: "2.3.1 (20200124)",
+  VERSION: "2.4.0 (20200707)",
   AUTHOR: "Isana Kashiwai",
   LICENSE: "MIT"
 };
