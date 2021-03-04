9adbcaa505e1999b034a539a6f00610dc6af70e8
KASHIWAI, Isana <isana.k@gmail.com>
2021-03-05 04:07:43 +0900

    remove unused console.log

--- build/orb-planetary.v2.js
+++ build/orb-planetary.v2.js
@@ -2165,9 +2165,7 @@ Orb.NutationAndObliquity = Orb.NutationAndObliquity || function (date) {
   var dt = time.delta_t();
   //jd = jd + (dt / 86400);
   var t = (jd - 2451545.0) / 36525;
-  console.log(t)
   var omega = 125.04452 - 1934.136261 * t + 0.0020708 * t * t + (t * t * t / 450000);
-  console.log(omega)
   var L0 = 280.4665 + 36000.7698 * t;
   var L1 = 218.3165 + 481267.8813 * t;
   var nutation = (-17.20 / 3600) * Math.sin(omega * rad) - (-1.32 / 3600) * Math.sin(2 * L0 * rad) - (0.23 / 3600) * Math.sin(2 * L1 * rad) + (0.21 / 3600) * Math.sin(2 * omega * rad);
