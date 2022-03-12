c3430bd4abe4a406feef19d34174527aa579c182
isnksw <isana.k@gmail.com>
2022-03-13 02:04:31 +0900

    Test for Time.jd()

--- test/app.js
+++ test/app.js
@@ -3,4 +3,7 @@ const Orb = require('../dist/orb.min.js');
 const date = new Date();
 const time = new Orb.Time(date);
 const time_in_day = time.time_in_day();
-console.log(time_in_day)
+console.log(`time in day: ${time_in_day}`);
+const jd = time.jd();
+console.log(`jd: ${jd}`);
+
